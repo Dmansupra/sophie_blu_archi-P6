dd86f863b824e68bd24c3024e79b0a1be7d81805
David Cabot <david.cabot@laposte.net>
2024-07-18 10:57:08 +0200

    correction

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -157,6 +157,9 @@ function removeToken() {
   sessionStorage.removeItem("deletedImages");
 }
 
+//événement fermeture onglet ou redirection vers un autre site
+window.addEventListener("unload", removeToken);
+
 // *****************************************************************************************************
 // *****************************DOM QUI PASSE EN MODE ADMIN EDITOR  ************************************
 // *****************************************************************************************************
@@ -165,7 +168,7 @@ function adminEdition() {
   // *****************************************************************************************************
   //*************************************OPEN 1ER MODAL EDIT SUPRESSION
   // *****************************************************************************************************
-  const modalJs = document.getElementById("titleemove");
+  const modalJs = document.getElementById("titleProjectRemove");
 
   modalJs.addEventListener("click", (e) => {
     e.preventDefault();
@@ -197,7 +200,7 @@ const adminHTML = () => {
 
   const spanFlagEditor = document.createElement("span");
   spanFlagEditor.classList.add("projectRemove");
-  spanFlagEditor.textContent = "Modifier";
+  spanFlagEditor.textContent = "Mode édition";
 
   //*************************************Créer Le SPAN avec le "i"
   const iconFlagEditor = document.createElement("i");
@@ -206,7 +209,11 @@ const adminHTML = () => {
   //*************************************Insérer l'élément i avant le texte de span
   spanFlagEditor.insertBefore(iconFlagEditor, spanFlagEditor.firstChild);
 
+  const btnFlagEditor = document.createElement("button");
+  btnFlagEditor.textContent = "publier les changements";
+
   flagEditor.appendChild(spanFlagEditor);
+  flagEditor.appendChild(btnFlagEditor);
 
   //*************************************Pointage des position à injecter
   const figure = document.querySelector("#introduction figure");
@@ -288,9 +295,6 @@ function openModal() {
     iconDelete.id = "deleteIcon";
     iconDelete.classList.add("fa-solid", "fa-trash-can", "iconModal");
     iconDelete.setAttribute("aria-hidden", "true");
-    iconDelete.addEventListener("click", () =>
-      functionDeleteWorksApi(cards[index].id)
-    );
     img.src = link;
     p.textContent = "éditer";
     container.appendChild(img);
@@ -362,27 +366,35 @@ function openModal() {
   const galleryMap = document.getElementById("modalGrid");
   galleryMap.append(...imageElements);
 }
-const functionDeleteWorksApi = async (id) => {
-  try {
-    if (token === false) return console.log({ error: "Pas connecté" });
+const functionDeleteWorksApi = () => {
+  // Récupérer la chaîne de sessionStorage
+  const deletedImagesJSON = sessionStorage.getItem("deletedImages");
+  // Convertir la chaîne en objet JavaScript
+  const deletedImages = JSON.parse(deletedImagesJSON);
+  // Supprimer chaque image du SESSION STORAGE
+  //méthode JavaScript qui renvoie un tableau contenant les clés d'un objet
+  Object.keys(deletedImages).forEach(async (id) => {
+    try {
+      if (token === false) return console.log({ error: "Pas connecté" });
 
-    const response = await fetch(`${api}works/${id}`, {
-      method: "DELETE",
-      headers: {
-        Accept: "*/*",
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    if (response.ok) {
-      console.log(`Image avec ID ${id} supprimée`);
-    } else {
-      throw new Error(response.statusText);
+      const response = await fetch(`${api}works/${id}`, {
+        method: "DELETE",
+        headers: {
+          Accept: "*/*",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (response.ok) {
+        console.log(`Image avec ID ${id} supprimée`);
+      } else {
+        throw new Error(response.statusText);
+      }
+    } catch (e) {
+      console.error(
+        `Erreur lors de la suppression de l'image avec ID ${id}: ${e}`
+      );
     }
-  } catch (e) {
-    console.error(
-      `Erreur lors de la suppression de l'image avec ID ${id}: ${e}`
-    );
-  }
+  });
 };
 // *****************************************************************************************************
 //**********************************  AFFICHAGE DE LA MODAL  *******************************************
@@ -515,22 +527,9 @@ function editModal() {
           console.log("Ta requête POST est passé :) :", data);
           fetchApiWorks();
           workDisplay();
-          document.querySelector("#title").value = "";
-          document.querySelector("#category").value = "";
-          document.querySelector("#addImageContainer").innerHTML = "";
-          document.querySelector(
-            "#addImageContainer"
-          ).innerHTML = `<i class="fa-solid fa-image"></i>
-
-            <div id="inputFile">
-              <label for="filetoUpload" class="fileLabel">
-                <span>+ Ajouter une photo</span>
-                <input type="file" id="filetoUpload" name="image" accept="image/png, image/jpeg"
-                  class="file-input">
-              </label>
-            </div>
-            <span class="filesize">jpg, png : 4mo max</span>
-            <span id="errorImg"></span>`;
+          closeModal();
+          // réinitialiser le champ inputFile sinon il envoie plusieur formData en post
+          inputFile.value = "";
         })
         .catch((error) => {
           console.error("Error:", error);
